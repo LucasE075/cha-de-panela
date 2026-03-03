5e22bd1b7ec4422bc4d801717e7edae73b4cacce
Lucas Emanuel <lucasemanuel075@gmail.com>
2026-03-03 17:41:23 -0300

    cha-de-panela1.3

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './src/app/App'
+import App from './app/App'
 
 import './styles/globals.css'
 import './styles/theme.css'
